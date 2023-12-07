3d17f3efd59b7eff3ff4fd10fe2b83661d63e9f4
gonzales <gonzalesitokawa@gmail.com>
2023-12-07 16:01:24 +0900

    component: add new component DisplayMarkdown #15

--- app/src/pages/articles/[article].tsx
+++ app/src/pages/articles/[article].tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
-import Markdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import remarkGfm from "remark-gfm";
 
+import useDarkMode from "@/components/layout/Header/hooks/useDarkMode";
 import { Layout } from "@/components/layout/Layout";
+import DisplayMarkdown from "@/components/lib/DisplayMarkdown";
 import Manager from "@/feature/Manager";
 import { IArticle, PrevAndNextArticleData } from "@/types/IArticle";
 
@@ -20,57 +18,13 @@ interface PathType {
 }
 
 const Content = ({ article, prev, next }: ArticleType) => {
+  const { theme } = useDarkMode();
+
   return (
     <Layout title="R'IndiCode">
       <main className="flex min-h-[calc(100vh-49px)] w-full flex-col overflow-x-hidden px-5 pb-8 pt-20 dark:bg-gray-900">
         <article className="mb-4">
-          <Markdown
-            components={{
-              h1(props) {
-                return (
-                  <div>
-                    <div className="mt-auto flex items-center gap-x-2 text-xs text-gray-400 dark:text-gray-400">
-                      <time className="">{article.createdAt}</time>
-                      <div className="h-1 w-1 rounded-full bg-gray-400 dark:bg-gray-500"></div>
-                      <p>{article.readingTime}分で読めます</p>
-                    </div>
-                    <h1 {...props} />
-                  </div>
-                );
-              },
-              img({ src, alt, ...props }) {
-                return (
-                  <img
-                    src={src}
-                    alt={alt}
-                    className="h-64 w-full object-cover"
-                    {...props}
-                  />
-                );
-              },
-              code(props) {
-                const { children, className, ...rest } = props;
-                const match = /language-(\w+)/.exec(className || "");
-                return match ? (
-                  <SyntaxHighlighter
-                    PreTag="div"
-                    language={match[1]}
-                    style={vscDarkPlus}
-                  >
-                    {String(children).replace(/\n$/, "")}
-                  </SyntaxHighlighter>
-                ) : (
-                  <code {...rest} className={className}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
-            className="prose prose-sm prose-gray max-w-none dark:prose-invert prose-pre:bg-white prose-pre:p-0 prose-thead:bg-gray-200 prose-th:px-2 prose-th:py-3 prose-td:px-2 dark:prose-pre:bg-gray-900 dark:prose-thead:bg-gray-800"
-            remarkPlugins={[remarkGfm]}
-          >
-            {article.content}
-          </Markdown>
+          <DisplayMarkdown article={article} theme={theme} />
         </article>
         <ul className="flex items-center justify-between gap-x-4 py-4">
           {prev && (
--- app/src/pages/snippets/[snippet].tsx
+++ app/src/pages/snippets/[snippet].tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
-import Markdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import remarkGfm from "remark-gfm";
 
+import useDarkMode from "@/components/layout/Header/hooks/useDarkMode";
 import { Layout } from "@/components/layout/Layout";
+import DisplayMarkdown from "@/components/lib/DisplayMarkdown";
 import Manager from "@/feature/SnippetManager";
 import { IArticle, PrevAndNextArticleData } from "@/types/IArticle";
 
@@ -20,56 +18,13 @@ interface PathType {
 }
 
 const Content = ({ snippet, prev, next }: SnippetType) => {
+  const { theme } = useDarkMode();
+
   return (
     <Layout title="R'IndiCode">
       <main className="flex min-h-[calc(100vh-49px)] w-full flex-col overflow-x-hidden px-5 pb-8 pt-20 dark:bg-gray-900">
         <article className="mb-4">
-          <Markdown
-            allowedElements={["h1", "pre", "code"]}
-            components={{
-              h1(props) {
-                return (
-                  <div>
-                    <div className="mt-auto flex items-center gap-x-2 text-xs text-gray-400 dark:text-gray-400">
-                      <time className="">{snippet.createdAt}</time>
-                    </div>
-                    <h1 {...props} />
-                  </div>
-                );
-              },
-              img({ src, alt, ...props }) {
-                return (
-                  <img
-                    src={src}
-                    alt={alt}
-                    className="h-64 w-full object-cover"
-                    {...props}
-                  />
-                );
-              },
-              code(props) {
-                const { children, className, ...rest } = props;
-                const match = /language-(\w+)/.exec(className || "");
-                return match ? (
-                  <SyntaxHighlighter
-                    PreTag="div"
-                    language={match[1]}
-                    style={vscDarkPlus}
-                  >
-                    {String(children).replace(/\n$/, "")}
-                  </SyntaxHighlighter>
-                ) : (
-                  <code {...rest} className={className}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
-            className="prose prose-sm prose-gray max-w-none dark:prose-invert prose-pre:bg-white prose-pre:p-0 prose-thead:bg-gray-200 prose-th:px-2 prose-th:py-3 prose-td:px-2 dark:prose-pre:bg-gray-900 dark:prose-thead:bg-gray-800"
-            remarkPlugins={[remarkGfm]}
-          >
-            {snippet.content}
-          </Markdown>
+          <DisplayMarkdown article={snippet} theme={theme} />
         </article>
         <ul className="flex items-center justify-between gap-x-4 py-4">
           {prev && (
